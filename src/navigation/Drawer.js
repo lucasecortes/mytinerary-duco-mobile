3dfd404bfe82dc679b63eafa88d5e74633b17748
lec89 <lucascorts@hotmail.com>
2022-09-27 21:11:02 +0200

    modified

--- src/navigation/Drawer.js
+++ src/navigation/Drawer.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import HomeScreen from '../Screens/HomeScreen';
-import EventScreen from '../Screens/EventScreen';
+import CityScreen from '../Screens/CityScreen';
 
 const DrawerNavigation = createDrawerNavigator();
 
 export default function drawer() {
   return (
-    <DrawerNavigation.Navigator>
+    <DrawerNavigation.Navigator initialRouteName='Home'>
         <DrawerNavigation.Screen name='Home' component={HomeScreen} />
-        <DrawerNavigation.Screen name='Home' component={EventScreen} />
+        <DrawerNavigation.Screen name='City' component={CityScreen} />
     </DrawerNavigation.Navigator>
   )
 }
