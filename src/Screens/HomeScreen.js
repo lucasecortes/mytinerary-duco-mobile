c2aad33e461874044a06e33dec4579fc33c43c1c
lec89 <lucascorts@hotmail.com>
2022-09-27 21:11:23 +0200

    modified

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, Text, Button } from 'react-native'
 
 export default function HomeScreen() {
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Button
+        onPress={() => navigation.goBack()} title="Go back home"
+        />
+        <Text>HomeScreen</Text>
     </View>
   )
 }
