baf928b89da819ed32155bdf8ff78a111c13febb
lec89 <lucascorts@hotmail.com>
2022-09-27 21:49:41 +0200

    drawer navigator working

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
+import * as React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import { Drawer } from '../mytinerary-ducore/src/navigation/Drawer'
+import HomeScreen from './src/Screens/HomeScreen';
+import CityScreen from './src/Screens/CityScreen';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer />
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Cities" component={CityScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
