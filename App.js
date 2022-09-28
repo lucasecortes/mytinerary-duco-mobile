537587af85520eb60cd5c970c6a07a6d43d01191
lec89 <lucascorts@hotmail.com>
2022-09-28 10:20:41 +0200

    screens added

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './src/Screens/HomeScreen';
 import CityScreen from './src/Screens/CityScreen';
+import SignIn from './src/Screens/SignIn';
+import SignUp from './src/Screens/SignUp';
 
 const Drawer = createDrawerNavigator();
 
@@ -12,6 +14,8 @@ export default function App() {
       <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen name="Home" component={HomeScreen} />
         <Drawer.Screen name="Cities" component={CityScreen} />
+        <Drawer.Screen name='SignIn' component={SignIn} />
+        <Drawer.Screen name='SignUp' component={SignUp} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
