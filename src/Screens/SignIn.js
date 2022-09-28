4faac4320b8db35138631be0b00bc8618ff2de2f
lec89 <lucascorts@hotmail.com>
2022-09-28 10:19:59 +0200

    sign in added

---
+++ src/Screens/SignIn.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SignIn({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  )
+}
