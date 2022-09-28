215f097eac965eb30bd915cfccd9b4d6e063cdb5
lec89 <lucascorts@hotmail.com>
2022-09-28 16:29:50 +0200

    provider modified

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
+import { Provider } from 'react-redux';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import store from './src/features/store';
 import HomeScreen from './src/Screens/HomeScreen';
 import CityScreen from './src/Screens/CityScreen';
 import SignIn from './src/Screens/SignIn';
@@ -11,12 +13,14 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Cities" component={CityScreen} />
-        <Drawer.Screen name='SignIn' component={SignIn} />
-        <Drawer.Screen name='SignUp' component={SignUp} />
-      </Drawer.Navigator>
+      <Provider store={store}>
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen name="Home" component={HomeScreen} />
+          <Drawer.Screen name="Cities" component={CityScreen} />
+          <Drawer.Screen name='Sign In' component={SignIn} />
+          <Drawer.Screen name='Sign Up' component={SignUp} />
+        </Drawer.Navigator>
+      </Provider>
     </NavigationContainer>
   );
 }
