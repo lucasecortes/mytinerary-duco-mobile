e0f05ebe021f2d535296b73229cecfbf621a5026
lec89 <lucascorts@hotmail.com>
2022-09-30 15:53:37 +0200

    city details beta

--- src/components/CityDetails.js
+++ src/components/CityDetails.js
@@ -1,10 +1,42 @@
-// import React from 'react'
-// import { View, Button } from 'react-native'
+import React from 'react'
+import { View, ScrollView, StyleSheet } from 'react-native'
+import Button from './Button'
+import theme from '../theme'
+import StyledText from './StyledText';
 
-// export default function CityDetails({ navigation }) {
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Button onPress={() => navigation.goBack()} title="Go back home" />
-//     </View>
-//   )
-// }
+export default function CityDetails(props) {
+  const cityDetails = props.data
+  return (
+    <View style={ styles.container}>
+      <ScrollView
+        style={{ height: 300, width: '100%' }}
+        horizontal
+        pagingEnabled>
+        {cityDetails?.map((value) => (
+          <View key={value._id}>
+            {/* <ImageBackground
+              source={{ uri: `${value.photo}` }} key={value._id}
+            >
+            </ImageBackground> */}
+            <StyledText>Country: {value.country}</StyledText>
+            <StyledText>City: {value.city}</StyledText>
+            <StyledText>Population: {value.population}</StyledText>
+            <StyledText>Founded: {value.founded}</StyledText>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+    // <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    //   <Button onPress={() => navigation.navigate('Cities')} title="Go to cities" />
+    // </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+      width: '100%',
+      height: '100%',
+      flex: 1,
+      backgroundColor: theme.colors.background
+  }
+  });
