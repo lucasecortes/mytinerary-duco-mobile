94dbb2f3a57668d57bede7358c71d5330b6e18aa
adrieldussex <adrieldussex@gmail.com>
2022-09-30 11:02:23 -0300

    Moddified navigation

--- src/navigation/Drawer.js
+++ src/navigation/Drawer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import HomeScreen from '../Screens/HomeScreen';
-import CityScreen from '../Screens/CityScreen';
+import CitiesScreen from '../Screens/CitiesScreen';
 
 const DrawerNavigation = createDrawerNavigator();
 
@@ -9,7 +9,7 @@ export default function Drawer() {
   return (
     <DrawerNavigation.Navigator initialRouteName='Home'>
         <DrawerNavigation.Screen name='Home' component={HomeScreen} />
-        <DrawerNavigation.Screen name='City' component={CityScreen} />
+        <DrawerNavigation.Screen name='Cities' component={CitiesScreen} />
     </DrawerNavigation.Navigator>
   )
 }
