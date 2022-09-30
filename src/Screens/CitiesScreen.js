a26a0c982432cb98992e2aba400c0172d84963e5
adrieldussex <adrieldussex@gmail.com>
2022-09-30 11:03:09 -0300

    Remove input, fix search

--- src/Screens/CitiesScreen.js
+++ src/Screens/CitiesScreen.js
@@ -1,18 +1,27 @@
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, TextInput } from 'react-native'
 import StyledText from '../components/StyledText'
 import Cities from '../components/Cities'
 import theme from '../theme'
-import Input from '../components/Input'
-import { useGetAllCitiesQuery } from '../features/citiesAPI'
+import { useSearchCitiesQuery } from '../features/citiesAPI'
+import { useState } from 'react'
 
 export default function CitiesScreen(){
-  let {data: cities} = useGetAllCitiesQuery()
+  let [search, setSearch]=useState('')
+  let {data: cities, isLoading, isSuccess} = useSearchCitiesQuery(search)
+  if (isLoading) {
+    cities=[]
+  } else if (isSuccess) {
+    cities=cities
+  }
 
 return (
     <View style={styles.container}>
       <StyledText fontWeight='bold' fontSize='title'>Cities</StyledText>
-      <Input/>
-      <Cities data={cities}/>
+      <TextInput
+        style={styles.input}
+        placeholder="Search"
+        onChangeText={newT=>setSearch(newT)}/>
+      <Cities data={cities} id={cities._id}/>
     </View>
   )
 }
@@ -26,4 +35,11 @@ const styles = StyleSheet.create({
     height: '100%',
     padding: 50
   },
+  input: {
+    width: '100%',
+    backgroundColor: theme.colors.textPrimary,
+    borderRadius: 24,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+}
 })
